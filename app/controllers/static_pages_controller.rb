93641750b5c923e60588a2843eaf7cf588ea4ea5
Barry Smyth <barry.smyth@gmail.com>
2012-07-07 11:48:06 +0100

    Finished static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
-class StaticPagesController < ApplicationController
+class StaticPagesController < ApplicationController                            
   def home
   end
 
   def help
+  end
+  
+  def about
   end
 end
